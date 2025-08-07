6171433ce659ca61c31ceb38e9c4e35f7cc127b5
J-saka0812 <J.saka0812@gmail.com>
2025-08-07 09:51:06 +0900

    1st_commit

---
+++ Main.java
@@ -0,0 +1,20 @@
+public class Main {
+	public static void main(String[] args) {
+		// String age = "31";
+		// int n = Integer.parseInt(age);
+		// System.out.println("私は今年" + (n + 1) + "の年齢になります");
+
+		// int r = new java.util.Random().nextInt(10);
+		// int l = new java.util.Random().nextInt(10);
+		// System.out.println(r + "\n" + l); 
+		// System.out.println(r / l + "余り" + r % l);
+
+		int a = 0;
+		while (a < 10) {
+			
+			System.out.println(a);
+			a++;
+		}
+
+	}
+}
